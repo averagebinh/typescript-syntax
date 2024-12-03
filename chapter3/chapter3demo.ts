1faafd380dd8413e14418f10900644743dab7fe9
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-03 14:47:25 +0700

    "default and optional params"

--- chapter3/chapter3demo.ts
+++ chapter3/chapter3demo.ts
@@ -2,7 +2,39 @@
 
 //lesson21: switch case
 //lesson 22: for loop
+//lesson 23: while with break loop
+//lesson25: keyword break
+//lesson 26: keyword continue
+//Chapter 4
+//lesson27: function
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+// function sum(a: number, b: number) {
+//   console.log('abc');
+// }
+
+//anonymous function
+// const sum2 = (a: number, b: number) => {
+//   return a + b;
+// };
+// console.log('check sum>>>, ', sum2(2, 4));
+
+// const sum3 = (x: number, y: number): number => {
+//   return x + y;
+// };
+
+// console.log('check sum3  ,', sum3(1, 10));
+
+//lesson 29 optional params
+// let sum4 = (x: number, y: number, z?: number) => {
+//   if (z) {
+//     return x + y + z;
+//   }
+//   return x + y;
+// };
+// console.log('check sum 4 ,', sum4(1, 2, 5));
+
+// let sum6 = (x: number, y: number, z = false) => {
+//   return x + y + z;
+// };
+// console.log('check sum 6 ,', sum6(1, 2), sum6(1, 2, 3));
+// default params
